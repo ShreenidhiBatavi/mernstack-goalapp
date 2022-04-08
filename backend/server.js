93281cf0313ef912bd18ad19f919ebc0eb9cdc58
shreenidhi Batavi <batavishrinidhi@gmail.com>
2022-04-08 13:06:12 +0530

    changes in server.js file

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ require('dotenv').config()
 const port=process.env.PORT||8000
 const connectDB=require('./config/db')
 const goals=require('./routes/goals')
+const users=require('./routes/user')
 connectDB()
 const app=express()
 app.use(express.json())
 
 app.use('/api/goals',goals)
+app.use('/api/users',users)
 app.listen(port,()=>{
     console.log(`port statrted at no  ${port}`)
 })
