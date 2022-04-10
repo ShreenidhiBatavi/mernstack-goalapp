22fd093cb29ba7f67ac53984dc576ae833225e80
shreenidhi Batavi <batavishrinidhi@gmail.com>
2022-04-10 21:36:52 +0530

    changes in user controllers

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 const express=require('express')
-require('dotenv').config()
+require("dotenv").config();
 const port=process.env.PORT||8000
 const connectDB=require('./config/db')
 const goals=require('./routes/goals')
 const users=require('./routes/user')
+const jwt=require('./middlewares/auth')
 connectDB()
+
 const app=express()
 app.use(express.json())
 
-app.use('/api/goals',goals)
+app.use('/api/goals',jwt.jwtVerify,goals)
 app.use('/api/users',users)
 app.listen(port,()=>{
     console.log(`port statrted at no  ${port}`)
