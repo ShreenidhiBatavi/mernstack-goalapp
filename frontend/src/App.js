eda82cc0f5395694b0b49980d6c353f72aa1f54f
shreenidhi Batavi <batavishrinidhi@gmail.com>
2022-05-16 11:28:44 +0530

    re added react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
-import Register from './pages/Register';
 
 function App() {
   return (
-    <>
-   <Router>
-      <div className='container'>
-        <Header/>
-          <Routes>
-              <Route path="/" element={<Dashboard/>}/>
-              <Route path="/login" element={<Login/>}/>
-              <Route path="/register" element={<Register/>}/>
-          </Routes>
-       </div>
-    </Router>
-    </>
-   
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
