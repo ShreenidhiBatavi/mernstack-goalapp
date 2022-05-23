15d03dc67f6318feddd84dc485f81fe2ad5e6c63
shreenidhi Batavi <batavishrinidhi@gmail.com>
2022-05-23 22:11:43 +0530

    App cmp changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Register from './components/Register';
+import Login from './components/Login';
+import Goal from './components/Goal';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProtectedRoutes from './ProtectedRoutes';
+import Modal from './components/Modal';
+import Menu from './components/Menu';
 
 function App() {
   return (
-    <div className="App">
+    <div className=" min-h-screen">
       
+      <Router>
+        <Routes>
+          
+            <Route path="/" element={<Login/>} />
+            <Route  path='/register' element={<Register/>}/>
+            <Route path="/dashboard" element={<ProtectedRoutes>
+               <Goal/>
+            </ProtectedRoutes>} />
+        </Routes>
+      </Router>
+       
     </div>
   );
 }
