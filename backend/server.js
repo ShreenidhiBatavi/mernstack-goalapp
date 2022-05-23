e738f4a3cd104d3e1abfd7fdee7e28af9756083e
shreenidhi Batavi <batavishrinidhi@gmail.com>
2022-05-23 22:11:04 +0530

    server changes

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,11 @@ const connectDB=require('./config/db')
 const goals=require('./routes/goals')
 const users=require('./routes/user')
 const jwt=require('./middlewares/auth')
+const cors=require('cors')
 connectDB()
 
 const app=express()
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/goals',jwt.jwtVerify,goals)
